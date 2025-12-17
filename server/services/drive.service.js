1a73fb1e69cde28cbd64251c080d8177b9d5e4f5
floatex-solar <mis@floatexsolar.com>
2025-12-17 16:31:44 +0530

    working till email sent

--- server/services/drive.service.js
+++ server/services/drive.service.js
@@ -1,7 +1,7 @@
 import { google } from "googleapis";
 import { Readable } from "stream";
-import appConfig from "../config/app-config.js";
 import { authManager } from "../utils/google-auth-manager.js";
+import appConfig from "../config/app-config.js";
 
 export async function uploadPdfToDrive({ pdfBuffer, fileName }) {
   const drive = google.drive({
@@ -9,13 +9,15 @@ export async function uploadPdfToDrive({ pdfBuffer, fileName }) {
     auth: authManager.getAuth(),
   });
 
+  const { RFQ_DRIVE_FOLDER_ID } = appConfig;
   const stream = Readable.from(pdfBuffer);
 
+  // 1. Upload file
   const res = await drive.files.create({
     supportsAllDrives: true,
     requestBody: {
       name: fileName,
-      parents: [appConfig.RFQ_DRIVE_FOLDER_ID],
+      parents: [RFQ_DRIVE_FOLDER_ID],
       mimeType: "application/pdf",
     },
     media: {
@@ -24,49 +26,15 @@ export async function uploadPdfToDrive({ pdfBuffer, fileName }) {
     },
   });
 
-  return {
+  // 2. Make it public (anyone with link)
+  await drive.permissions.create({
+    supportsAllDrives: true,
     fileId: res.data.id,
-    viewUrl: `https://drive.google.com/file/d/${res.data.id}/view`,
-  };
+    requestBody: {
+      role: "reader",
+      type: "anyone",
+    },
+  });
+
+  return `https://drive.google.com/file/d/${res.data.id}/view`;
 }
-
-// import { google } from "googleapis";
-// import { Readable } from "stream";
-// import { authManager } from "../utils/google-auth-manager.js";
-// import appConfig from "../config/app-config.js";
-
-// export async function uploadPdfToDrive({ pdfBuffer, fileName }) {
-//   const drive = google.drive({
-//     version: "v3",
-//     auth: authManager.getAuth(),
-//   });
-
-//   const { RFQ_DRIVE_FOLDER_ID } = appConfig;
-//   const stream = Readable.from(pdfBuffer);
-
-//   // 1. Upload file
-//   const res = await drive.files.create({
-//     supportsAllDrives: true,
-//     requestBody: {
-//       name: fileName,
-//       parents: [RFQ_DRIVE_FOLDER_ID],
-//       mimeType: "application/pdf",
-//     },
-//     media: {
-//       mimeType: "application/pdf",
-//       body: stream,
-//     },
-//   });
-
-//   // 2. Make it public (anyone with link)
-//   await drive.permissions.create({
-//     supportsAllDrives: true,
-//     fileId: res.data.id,
-//     requestBody: {
-//       role: "reader",
-//       type: "anyone",
-//     },
-//   });
-
-//   return `https://drive.google.com/file/d/${res.data.id}/view`;
-// }
